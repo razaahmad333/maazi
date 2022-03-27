da75929ddf8df6c24602abf7f8dfcd5cf34388f9
razaahmad333 <raza82504@gmail.com>
2022-03-27 12:57:59 +0530

    alive or not

--- aliens.js
+++ aliens.js
@@ -2,27 +2,32 @@ const aliens = [
     {
         name: 'Alien 1',
         color: 'red',
-        points: 10
+        points: 10,
+        isAlive: true
     },
     {
         name: 'Alien 2',
         color: 'green',
-        points: 20
+        points: 20,
+        isAlive: true
     },
     {
         name: 'Alien 3',
         color: 'blue',
-        points: 30
+        points: 30,
+        isAlive: true
     },
     {
         name: 'Alien 4',
         color: 'yellow',
-        points: 40
+        points: 40,
+        isAlive: true
     },
     {
         name: 'Alien 5',
         color: 'purple blue violet',
-        points: 50
+        points: 50,
+        isAlive: true
     }
     
 ]
