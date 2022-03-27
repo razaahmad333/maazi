017ac8c8c08a039f0d68b232a2b3b12bd974c2d4
razaahmad333 <raza82504@gmail.com>
2022-03-27 13:01:56 +0530

    Revert "alive or not"
    This reverts commit da75929ddf8df6c24602abf7f8dfcd5cf34388f9.

--- aliens.js
+++ aliens.js
@@ -2,32 +2,27 @@ const aliens = [
     {
         name: 'Alien 1',
         color: 'red',
-        points: 10,
-        isAlive: true
+        points: 10
     },
     {
         name: 'Alien 2',
         color: 'green',
-        points: 20,
-        isAlive: true
+        points: 20
     },
     {
         name: 'Alien 3',
         color: 'blue',
-        points: 30,
-        isAlive: true
+        points: 30
     },
     {
         name: 'Alien 4',
         color: 'yellow',
-        points: 40,
-        isAlive: true
+        points: 40
     },
     {
         name: 'Alien 5',
         color: 'purple blue violet',
-        points: 50,
-        isAlive: true
+        points: 50
     }
     
 ]
