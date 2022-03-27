b39820ec71ad6ef38541408fa0ff087e4ee9db5e
razaahmad333 <raza82504@gmail.com>
2022-03-27 10:35:50 +0530

    my def razaahmad

--- big-bang.py
+++ big-bang.py
@@ -3,6 +3,12 @@ name = 'Big Bang'
 description = "This where our universe starts"
 
 dateOfEvent = '1/1/1/1'
+def getWhoCause():
+    for i in range(1, 100):
+        print(i)
+    return 'God'
+causer = getWhoCause()
+
 
 causer = getWhoCause()
 
