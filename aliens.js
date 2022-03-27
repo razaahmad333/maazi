5113764f662ce5c36df412603ded7dddb8d3b3f3
razaahmad333 <raza82504@gmail.com>
2022-03-27 11:14:06 +0530

    aliens added

---
+++ aliens.js
@@ -0,0 +1,28 @@
+const aliens = [
+    {
+        name: 'Alien 1',
+        color: 'red',
+        points: 10
+    },
+    {
+        name: 'Alien 2',
+        color: 'green',
+        points: 20
+    },
+    {
+        name: 'Alien 3',
+        color: 'blue',
+        points: 30
+    },
+    {
+        name: 'Alien 4',
+        color: 'yellow',
+        points: 40
+    },
+    {
+        name: 'Alien 5',
+        color: 'purple blue violet',
+        points: 50
+    }
+    
+]
