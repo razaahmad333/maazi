53c3242ff4bfe2778731e0bbce02a99ecab8072b
razaahmad333 <raza82504@gmail.com>
2022-03-31 14:19:14 +0530

    issue comment workflow deployed

--- dinosour.ts
+++ dinosour.ts
@@ -1,3 +1,29 @@
+interface IDinosaur {
+  name: string;
+  type: string;
+  age: number;
+  isCarnivore: boolean;
+  isExtinct: boolean;
+  isEndangered: boolean;
+  isHerbivore: boolean;
+  isOmnivore: boolean;
+  isPiscine: boolean;
+  isReptile: boolean;
+  isSauropod: boolean;
+  isSpine: boolean;
+  isTriceratops: boolean;
+  isTyrannosaurus: boolean;
+  isVelociraptor: boolean;
+  isVulgaris: boolean;
+  isWater: boolean;
+  isWyvern: boolean;
+  isZooplankton: boolean;
+  isZygodactyl: boolean;
+  isZygodactylus: boolean;
+  isZygomorpha: boolean;
+  isZygoraptor: boolean;
+}
+
 const nam_ = "Dinosour";
 const description = "In this age dinousr were alive and ruled the earth";
 
